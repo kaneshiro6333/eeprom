8fb592a6a72a917919aa7d995581bd8311fccf46
kaneshiro6333 <111410957+kaneshiro6333@users.noreply.github.com>
2023-09-18 11:57:47 +1000

    Add files via upload
    eeprom lib

---
+++ eeprom.h
@@ -0,0 +1,27 @@
+#ifndef EEPROM_H_
+#define EEPROM_H_
+
+#include <string.h>
+#include "config.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+
+#define EEPROM_SIZE 1023
+
+
+enum{
+	EEPROM_OK,
+	EEPROM_WRITE_FAIL,
+	EEPROM_INVALID_ADDR	
+};
+
+
+uint8_t EEPROM_read(uint16_t uiAddress, uint8_t *data);
+uint8_t EEPROM_write(uint16_t uiAddress, uint8_t ucData);
+uint8_t EEPROM_update(uint16_t uiAddress, uint8_t ucData);
+uint8_t EEPROM_update_batch(uint16_t uiAddress, void *data,uint16_t len);
+uint8_t EEPROM_read_batch(uint16_t uiAddress, void *data,uint16_t len);
+
+
+#endif /* EEPROM_H_ */
